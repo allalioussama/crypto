28779c340866551b4ea3711b7c981325ee8d9e2b
allalioussama <48457582+allalioussama@users.noreply.github.com>
2019-03-19 12:29:04 -0400

    Update read.py
    exemple of cesar cipher : convert a bin file content to  table  of hex than decrypt the table

--- read.py
+++ read.py
@@ -2,7 +2,8 @@
 
 import binascii
 
-with open('ch7.bin','rb') as f:
+
+with open('binaryfile.bin','rb') as f:
 		hexdata = binascii.hexlify(f.read())
 		hexlist = map(''.join, zip(hexdata[::2], hexdata[1::2]))
 
@@ -11,3 +12,5 @@ for i in range(256):
 	result=''.join(message)
 	print('%i : %s' % (i,result))
 
+#ord() function in python return an integer representing the Unicode code  of the character .
+#it 'is the reverse of chr() :example ord('a')=97:chr(97)=a
